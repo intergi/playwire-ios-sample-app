35ad749f2883aa52ff476eff1ba16131ef62104d
EnoshPW <emosuganti@playwire.com>
2026-04-16 16:28:50 -0400

    fix(PlaywireSwiftUI): resolve MAX mediation debugger popup on launch

--- PlaywireSwiftUI/PlaywireSwiftUIApp.swift
+++ PlaywireSwiftUI/PlaywireSwiftUIApp.swift
@@ -8,16 +8,27 @@
 import Playwire
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    @objc var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        PWNotifier.shared.startConsoleLogger()
+        PlaywireSDK.shared.test = false
+        return true
+    }
+}
+
 @main
 struct PlaywireSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             AdTypesView()
-                .onFirstAppear {
-                    PWNotifier.shared.startConsoleLogger()
-                    PlaywireSDK.shared.test = false
-                }
-
         }
     }
 }
